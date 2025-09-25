0aa9d4713c46da346c3e6bd37beffc663cc88587
jarvispact <jarvispact@gmail.com>
2025-09-25 18:12:33 +0200

    add defineComponentTypes to ecs package.

--- packages/ecs/src/component.ts
+++ packages/ecs/src/component.ts
@@ -1,3 +1,26 @@
+import { Range, Increment, ComponentTypes } from './utils';
+
+const MAX_COMPONENT_TYPES = 32;
+type MaxComponentTypes = typeof MAX_COMPONENT_TYPES;
+type MaxComponentTypesRange = Range<Increment<MaxComponentTypes>>;
+
+export const defineComponentTypes = <const T extends string[]>(
+    typeNames: T,
+): T['length'] extends MaxComponentTypesRange
+    ? ComponentTypes<T>
+    : `ERR: Only ${MaxComponentTypes} component types supported.` => {
+    if (typeNames.length > MAX_COMPONENT_TYPES) return {} as never;
+
+    const componentTypes: Record<string, number> = {};
+
+    for (let i = 0; i < typeNames.length; i++) {
+        const key = typeNames[i];
+        componentTypes[key] = i;
+    }
+
+    return componentTypes as never;
+};
+
 export type Component<ComponentType extends number = number, Data = undefined> = Data extends undefined
     ? { type: ComponentType }
     : { type: ComponentType; data: Data };
