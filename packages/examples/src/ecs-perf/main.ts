a8d1f4bee3eb78e1815dbf9c0d18707df0533168
jarvispact <jarvispact@gmail.com>
2025-09-26 18:44:12 +0200

    add performance comparison old vs new.

--- packages/examples/src/ecs-perf/main.ts
+++ packages/examples/src/ecs-perf/main.ts
@@ -135,41 +135,75 @@ const despawnTime = `${(despawnT1 - despawnT0).toFixed(4)}ms`;
 console.log(`despawn ${ENTITY_COUNT} entities in ${despawnTime}`);
 
 /**
+Old system:
+Spawned 10000 entities in 28.5500ms
+add 2 components to 10000 entities in 10.3900ms
+remove 2 components from 10000 entities in 21.3700ms
+despawn 10000 entities in 5.3900ms
+
+Spawned 10000 entities in 27.8400ms
+add 2 components to 10000 entities in 12.7400ms
+remove 2 components from 10000 entities in 13.2300ms
+despawn 10000 entities in 5.0750ms
+
+Spawned 10000 entities in 17.3150ms
+add 2 components to 10000 entities in 15.3450ms
+remove 2 components from 10000 entities in 12.4800ms
+despawn 10000 entities in 11.5500ms
+
+Spawned 10000 entities in 10.0850ms
+add 2 components to 10000 entities in 13.6800ms
+remove 2 components from 10000 entities in 8.3550ms
+despawn 10000 entities in 5.1800ms
+
+Spawned 10000 entities in 7.9600ms
+add 2 components to 10000 entities in 5.2650ms
+remove 2 components from 10000 entities in 21.4050ms
+despawn 10000 entities in 4.8900ms
+
+Spawned 10000 entities in 7.8750ms
+add 2 components to 10000 entities in 4.8650ms
+remove 2 components from 10000 entities in 17.1000ms
+despawn 10000 entities in 4.8350ms
+
+spawn = 28 + 27 + 17 + 10 + 8 + 8 = 98 = avg [16]
+add = 10 + 12 + 15 + 13 + 5 + 4 = 59 = avg [10]
+remove = 21 + 13 + 12 + 8 + 21 + 17 = 92 = avg [15]
+despawn = 5 + 5 + 11 + 5 + 4 + 4 = 34 = avg [5]
+
 New system:
+Spawned 10000 entities in 18.0500ms
+add 2 components to 10000 entities in 13.7050ms
+remove 2 components from 10000 entities in 2.7050ms
+despawn 10000 entities in 14.3400ms
 
-Spawned 10000 entities in 20.9700ms
-add 2 components to 10000 entities in 9.6400ms
+Spawned 10000 entities in 12.2050ms
+add 2 components to 10000 entities in 15.5600ms
+remove 2 components from 10000 entities in 2.8150ms
+despawn 10000 entities in 6.4350ms
+
+Spawned 10000 entities in 8.1600ms
+add 2 components to 10000 entities in 17.2300ms
 remove 2 components from 10000 entities in 2.8350ms
-Ran 3 systems with avg time: 0.1654ms
-despawn 10000 entities in 6.1700ms
+despawn 10000 entities in 5.8700ms
 
-Spawned 10000 entities in 20.3750ms
-add 2 components to 10000 entities in 15.5750ms
-remove 2 components from 10000 entities in 2.7300ms
-Ran 3 systems with avg time: 0.2304ms
-despawn 10000 entities in 6.1300ms
+Spawned 10000 entities in 16.2800ms
+add 2 components to 10000 entities in 7.4100ms
+remove 2 components from 10000 entities in 3.2350ms
+despawn 10000 entities in 8.0650ms
 
-Spawned 10000 entities in 17.0550ms
-add 2 components to 10000 entities in 8.4500ms
-remove 2 components from 10000 entities in 3.6950ms
-Ran 3 systems with avg time: 0.2859ms
-despawn 10000 entities in 13.3750ms
+Spawned 10000 entities in 10.9000ms
+add 2 components to 10000 entities in 15.9600ms
+remove 2 components from 10000 entities in 2.2750ms
+despawn 10000 entities in 5.7850ms
 
-Spawned 10000 entities in 12.8550ms
-add 2 components to 10000 entities in 10.3050ms
-remove 2 components from 10000 entities in 2.5150ms
-Ran 3 systems with avg time: 0.1707ms
-despawn 10000 entities in 6.7650ms
+Spawned 10000 entities in 10.7550ms
+add 2 components to 10000 entities in 21.6600ms
+remove 2 components from 10000 entities in 2.4550ms
+despawn 10000 entities in 5.7750ms
 
-Spawned 10000 entities in 8.1000ms
-add 2 components to 10000 entities in 16.1250ms
-remove 2 components from 10000 entities in 2.3400ms
-Ran 3 systems with avg time: 0.1820ms
-despawn 10000 entities in 5.8400ms
-
-Spawned 10000 entities in 16.2850ms
-add 2 components to 10000 entities in 7.8500ms
-remove 2 components from 10000 entities in 3.1600ms
-Ran 3 systems with avg time: 0.3030ms
-despawn 10000 entities in 8.0550ms
+spawn = 18 + 12 + 8 + 16 + 11 + 10 = 75 / [12.5]
+add = 13 + 15 + 17 + 7 + 15 + 21 = 88 / [14.6]
+remove = 3 + 2 + 2 + 3 + 2 + 2 = 14 / [2.3]
+despawn = 14 + 6 + 5 + 8 + 5 + 6 = 44 / [7.3]
  */
