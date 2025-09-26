27f1dccfbeca286ad18e36056f5fcc5e6ed9db2d
jarvispact <jarvispact@gmail.com>
2025-09-26 18:26:07 +0200

    cleanup logs and comment.

--- packages/examples/src/ecs-perf/main.ts
+++ packages/examples/src/ecs-perf/main.ts
@@ -33,10 +33,6 @@ const ENTITY_COUNT = 10_000;
 const SYSTEM_RUNS = 100;
 const entities: number[] = [];
 
-console.log(posOnly.length);
-console.log(velOnly.length);
-console.log(movable.length);
-
 // spawn test
 
 const spawnT0 = performance.now();
@@ -47,10 +43,6 @@ for (let i = 0; i < ENTITY_COUNT; i++) {
 const spawnT1 = performance.now();
 const spawnTime = `${(spawnT1 - spawnT0).toFixed(4)}ms`;
 console.log(`Spawned ${ENTITY_COUNT} entities in ${spawnTime}`);
-
-console.log(posOnly.length);
-console.log(velOnly.length);
-console.log(movable.length);
 
 // addComponent test
 
@@ -63,10 +55,6 @@ for (let i = 0; i < entities.length; i++) {
 const addCompT1 = performance.now();
 const addCompTime = `${(addCompT1 - addCompT0).toFixed(4)}ms`;
 console.log(`add 2 components to ${entities.length} entities in ${addCompTime}`);
-
-console.log(posOnly.length);
-console.log(velOnly.length);
-console.log(movable.length);
 
 // removeComponent test
 
@@ -84,10 +72,6 @@ const removeCompTime = `${(removeCompT1 - removeCompT0).toFixed(4)}ms`;
 console.log(`remove 2 components from ${entities.length} entities in ${removeCompTime}`);
 
 // system test
-
-console.log(posOnly.length);
-console.log(velOnly.length);
-console.log(movable.length);
 
 const system1 = () => {
     for (let i = 0; i < posOnly.length; i++) {
@@ -134,77 +118,39 @@ console.log(`despawn ${entities.length} entities in ${despawnTime}`);
 /**
 Results old:
 
-Spawned 10000 entities in 25.9350ms
-add 2 components to 10000 entities in 16.8900ms
-remove 2 components from 10000 entities in 17.3500ms
-Ran 3 systems with avg time: 0.0007ms
-despawn 10000 entities in 5.6700ms
+Spawned 10000 entities in 28.5500ms
+add 2 components to 10000 entities in 10.3900ms
+remove 2 components from 10000 entities in 21.3700ms
+Ran 3 systems with avg time: 0.0009ms
+despawn 10000 entities in 5.3900ms
 
-Spawned 10000 entities in 19.7250ms
-add 2 components to 10000 entities in 19.0750ms
-remove 2 components from 10000 entities in 19.9750ms
-Ran 3 systems with avg time: 0.0012ms
-despawn 10000 entities in 9.7250ms
-
-Spawned 10000 entities in 14.9950ms
-add 2 components to 10000 entities in 5.4700ms
-remove 2 components from 10000 entities in 10.8700ms
-Ran 3 systems with avg time: 0.0006ms
-despawn 10000 entities in 9.6550ms
-
-Spawned 10000 entities in 28.2550ms
-add 2 components to 10000 entities in 11.0600ms
-remove 2 components from 10000 entities in 13.7350ms
+Spawned 10000 entities in 27.8400ms
+add 2 components to 10000 entities in 12.7400ms
+remove 2 components from 10000 entities in 13.2300ms
 Ran 3 systems with avg time: 0.0008ms
-despawn 10000 entities in 5.5250ms
+despawn 10000 entities in 5.0750ms
 
-Spawned 10000 entities in 26.3950ms
-add 2 components to 10000 entities in 7.0200ms
-remove 2 components from 10000 entities in 25.8700ms
-Ran 3 systems with avg time: 0.0014ms
-despawn 10000 entities in 9.4200ms
+Spawned 10000 entities in 17.3150ms
+add 2 components to 10000 entities in 15.3450ms
+remove 2 components from 10000 entities in 12.4800ms
+Ran 3 systems with avg time: 0.0007ms
+despawn 10000 entities in 11.5500ms
 
-Spawned 10000 entities in 27.0000ms
-add 2 components to 10000 entities in 12.8650ms
-remove 2 components from 10000 entities in 21.5100ms
-Ran 3 systems with avg time: 0.0016ms
-despawn 10000 entities in 8.3350ms
+Spawned 10000 entities in 10.0850ms
+add 2 components to 10000 entities in 13.6800ms
+remove 2 components from 10000 entities in 8.3550ms
+Ran 3 systems with avg time: 0.0001ms
+despawn 10000 entities in 5.1800ms
 
-Results new:
+Spawned 10000 entities in 7.9600ms
+add 2 components to 10000 entities in 5.2650ms
+remove 2 components from 10000 entities in 21.4050ms
+Ran 3 systems with avg time: 0.0001ms
+despawn 10000 entities in 4.8900ms
 
-Spawned 10000 entities in 29.6500ms
-add 2 components to 10000 entities in 48.6550ms
-remove 2 components from 10000 entities in 1.8200ms
-Ran 3 systems with avg time: 0.2835ms
-despawn 10000 entities in 3.1700ms
-
-Spawned 10000 entities in 56.2000ms
-add 2 components to 10000 entities in 41.7700ms
-remove 2 components from 10000 entities in 1.9200ms
-Ran 3 systems with avg time: 0.1950ms
-despawn 10000 entities in 3.2100ms
-
-Spawned 10000 entities in 34.0250ms
-add 2 components to 10000 entities in 48.9400ms
-remove 2 components from 10000 entities in 1.9650ms
-Ran 3 systems with avg time: 0.2623ms
-despawn 10000 entities in 3.2200ms
-
-Spawned 10000 entities in 36.2200ms
-add 2 components to 10000 entities in 42.2100ms
-remove 2 components from 10000 entities in 2.1450ms
-Ran 3 systems with avg time: 0.2542ms
-despawn 10000 entities in 7.9750ms
-
-Spawned 10000 entities in 61.1550ms
-add 2 components to 10000 entities in 52.2950ms
-remove 2 components from 10000 entities in 1.7000ms
-Ran 3 systems with avg time: 0.1881ms
-despawn 10000 entities in 3.2150ms
-
-Spawned 10000 entities in 44.7850ms
-add 2 components to 10000 entities in 44.7250ms
-remove 2 components from 10000 entities in 1.6200ms
-Ran 3 systems with avg time: 0.3096ms
-despawn 10000 entities in 2.9950ms
+Spawned 10000 entities in 7.8750ms
+add 2 components to 10000 entities in 4.8650ms
+remove 2 components from 10000 entities in 17.1000ms
+Ran 3 systems with avg time: 0.0001ms
+despawn 10000 entities in 4.8350ms
  */
