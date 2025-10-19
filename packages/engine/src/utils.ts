f4dbbb24a0b9656d4761ad630cc2375ef93fc245
jarvispact <jarvispact@gmail.com>
2025-10-19 16:04:55 +0200

    Add some utils to engine.

---
+++ packages/engine/src/utils.ts
@@ -0,0 +1,16 @@
+export const ImageLoader = {
+    loadImage: (url: string, options?: ImageBitmapOptions) =>
+        fetch(url)
+            .then((res) => res.blob())
+            .then((blob) => createImageBitmap(blob, options)),
+};
+
+export const DomUtils = {
+    getCanvasById: (id: string) => {
+        const dpr = window.devicePixelRatio || 1;
+        const canvas = document.getElementById(id) as HTMLCanvasElement;
+        canvas.width = canvas.clientWidth * dpr;
+        canvas.height = canvas.clientHeight * dpr;
+        return canvas;
+    },
+};
