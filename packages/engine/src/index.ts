ab9d528fdeb445075dab3f5816e0cbf323bb4a12
jarvispact <jarvispact@gmail.com>
2026-02-09 21:24:45 +0100

    working hot reloading

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -1,3 +1,14 @@
+import { gltf2 } from '@timefold/gltf2';
+import { math } from '@timefold/math';
+import { obj } from '@timefold/obj';
+import { webgpu } from '@timefold/webgpu';
+
 export const engine = (test: string) => {
-    return { engine: test };
+    return {
+        engine: test,
+        m: math('Hello from math!'),
+        g: gltf2('Hello from gltf2!'),
+        o: obj('Hello from obj!'),
+        w: webgpu('Hello from webgpu!'),
+    };
 };
--- packages/examples/vite.config.ts
+++ packages/examples/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, Plugin, PreviewServer, ViteDevServer } from 'vite';
+import path from 'node:path';
 
 const setSharedArrayBufferHeaders = (server: ViteDevServer | PreviewServer) => {
     server.middlewares.use((_req, res, next) => {
@@ -17,16 +18,32 @@ const sharedArrayBufferHeaderPlugin = (): Plugin => ({
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [sharedArrayBufferHeaderPlugin()],
+    resolve: {
+        alias: {
+            '@timefold/ecs': path.resolve(__dirname, '../ecs/src'),
+            '@timefold/math': path.resolve(__dirname, '../math/src'),
+            '@timefold/webgpu': path.resolve(__dirname, '../webgpu/src'),
+            '@timefold/obj': path.resolve(__dirname, '../obj/src'),
+            '@timefold/gltf2': path.resolve(__dirname, '../gltf2/src'),
+            '@timefold/engine': path.resolve(__dirname, '../engine/src'),
+        },
+    },
+    optimizeDeps: {
+        exclude: [
+            '@timefold/ecs',
+            '@timefold/math',
+            '@timefold/webgpu',
+            '@timefold/obj',
+            '@timefold/gltf2',
+            '@timefold/engine',
+        ],
+    },
+    server: {
+        watch: {
+            ignored: ['!**/node_modules/@timefold/**'],
+        },
+    },
     build: {
         target: 'esnext',
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules/')) {
-                        return 'vendor';
-                    }
-                },
-            },
-        },
     },
 });
