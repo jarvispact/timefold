1226c107a9360af5c9d6513fd9cf8e0c643e580c
jarvispact <jarvispact@gmail.com>
2026-03-23 19:27:47 +0100

    fix wrong values in test setup

--- packages/webgpu/test-setup.ts
+++ packages/webgpu/test-setup.ts
@@ -1,3 +1,16 @@
 import { vi } from 'vitest';
 
-vi.stubGlobal('GPUShaderStage', { VERTEX: 0, FRAGMENT: 1, COMPUTE: 2 });
+vi.stubGlobal('GPUShaderStage', { VERTEX: 0x1, FRAGMENT: 0x2, COMPUTE: 0x4 });
+
+vi.stubGlobal('GPUBufferUsage', {
+    MAP_READ: 0x0001,
+    MAP_WRITE: 0x0002,
+    COPY_SRC: 0x0004,
+    COPY_DST: 0x0008,
+    INDEX: 0x0010,
+    VERTEX: 0x0020,
+    UNIFORM: 0x0040,
+    STORAGE: 0x0080,
+    INDIRECT: 0x0100,
+    QUERY_RESOLVE: 0x0200,
+});
